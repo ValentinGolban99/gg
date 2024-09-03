478acebed61d950389912ce5107cd87c792945b3
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2024-09-03 11:43:42 +0300

    Создал класс и точку входа

---
+++ program.java
@@ -0,0 +1,17 @@
+/**
+ * Создать класс program - ввод "class"(это называется снипед)
+ * 
+ * Снипед точка входа - "main"
+ * 
+ * Снипед обращение к консоли - "sysout"
+ * 
+ * F5 - запуск программы(также можно через "run")
+ * 
+ * 
+ */
+public class program {
+   public static void main(String[] args) {
+       System.out.println("--Пример текста--");
+   }
+    
+}
